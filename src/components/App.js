b594f7764de15a6b739914cd8a3a86205cab09ed
daxas-boop <daxas.swd@hotmail.com>
2020-09-25 00:33:51 -0300

    Adds a basic layout for the page

---
+++ src/components/App.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route, NavLink} from 'react-router-dom';
+import styled from '@emotion/styled'
+import Inicio from './pages/Inicio'
+import Menu from './pages/Menu'
+import Contacto from './pages/Contacto'
+import Acerca from './pages/Acerca'
+import Header from './Header'
+import Footer from './Footer'
+import Background from '../images/background.png'
+
+const Container = styled.div `
+  background-color: #cccccc;
+  background-image: url(${Background});
+  background-repeat: repeat;
+  background-attachment: fixed;
+  background-size: 100% 100%;
+  background-position: center;
+  font-family: Arial, Helvetica, sans-serif;
+  box-sizing: border-box;
+`
+const Wrapper = styled.div`
+  padding-bottom: 45px;
+  max-width: 1080px;
+  margin: 0 auto;
+  min-height: 100vh;
+  position: relative;
+  box-sizing: border-box;
+`
+
+function App() {
+  return (
+    <Container>
+      <Wrapper>
+        <Router>
+          <Header></Header>
+            <Switch>
+              <Route path='/Inicio' exact>
+                <Inicio></Inicio>
+              </Route>
+              <Route path='/Menu'>
+                <Menu></Menu>
+              </Route>
+              <Route path='/Contacto'>
+                <Contacto></Contacto>
+              </Route>
+              <Route path='/Acerca'>
+                <Acerca></Acerca>
+              </Route>
+            </Switch>
+          <Footer></Footer>
+        </Router>
+      </Wrapper>
+    </Container>
+  );
+}
+
+export default App;
