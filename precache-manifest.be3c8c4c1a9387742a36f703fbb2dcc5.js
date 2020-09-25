cc637b30b95cc664e841d054cb9fcc422da6910d
daxas-boop <daxas.swd@hotmail.com>
2020-09-25 00:42:27 -0300

    Updates

---
+++ precache-manifest.be3c8c4c1a9387742a36f703fbb2dcc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa993b0d2f649f8fb7539aea76d61672",
+    "url": "/restaurant-page-react/index.html"
+  },
+  {
+    "revision": "ec4328886fe21d673de0",
+    "url": "/restaurant-page-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1f8e7ea846169f7e5bac",
+    "url": "/restaurant-page-react/static/js/2.bec3c6b8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/restaurant-page-react/static/js/2.bec3c6b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec4328886fe21d673de0",
+    "url": "/restaurant-page-react/static/js/main.17f96fef.chunk.js"
+  },
+  {
+    "revision": "24491bcd7b902f0ff46e",
+    "url": "/restaurant-page-react/static/js/runtime-main.ec1bc154.js"
+  },
+  {
+    "revision": "e84666885a7e549f16d4f6dbc4327061",
+    "url": "/restaurant-page-react/static/media/background.e8466688.png"
+  }
+]);
